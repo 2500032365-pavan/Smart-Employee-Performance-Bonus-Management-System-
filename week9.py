9765335e48c48638adf1a44fce73ebac8e18a762
2500032365-pavan <potharajupavan007@gmail.com>
2026-04-28 09:04:03 +0530

    week9.py

---
+++ week9.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+ratings = np.array([4.5,3.8,4.9,2.7,3.5])
+
+
+print("Average Rating:", np.mean(ratings))
+
+print("Max Rating:", np.max(ratings))
+
+print("Min Rating:", np.min(ratings))
