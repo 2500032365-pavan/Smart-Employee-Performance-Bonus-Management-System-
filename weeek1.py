a546acfb4aab4663bf8de48ecaccd223cddae72f
2500032365-pavan <potharajupavan007@gmail.com>
2026-04-28 08:58:19 +0530

    week1.py

---
+++ weeek1.py
@@ -0,0 +1,13 @@
+name = input("Enter Employee Name: ")
+emp_id = input("Enter Employee ID: ")
+department = input("Enter Department: ")
+salary = float(input("Enter Salary: "))
+rating = float(input("Enter Performance Rating (1-5): "))
+
+print("\nEmployee Details")
+print(f"Name: {name}")
+print(f"ID: {emp_id}")
+print(f"Department: {department}")
+print(f"Salary: {salary}")
+print(f"Rating: {rating}")
+Week 1
